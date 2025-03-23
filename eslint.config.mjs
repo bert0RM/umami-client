a9be14f0eb1b94f6ebe0f47061028042714ecd8f
Rigo <33237379+bert0RM@users.noreply.github.com>
2025-03-23 10:22:44 -0700

    chore(deps): remove unused deps (#45)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,18 +2,16 @@ import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
 import prettierPlugin from 'eslint-plugin-prettier/recommended';
 import importPlugin from 'eslint-plugin-import';
-import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import jestPlugin from 'eslint-plugin-jest';
+import promise from 'eslint-plugin-promise';
 
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
-      parser: '@typescript-eslint/parser',
       ecmaVersion: 9,
       globals: {
         ...globals.browser,
@@ -23,9 +21,8 @@ export default defineConfig([
       },
     },
   },
-  reactPlugin.configs.flat.recommended,
   importPlugin.flatConfigs.react,
-  reactHooksPlugin.configs['recommended-latest'],
+  promise.configs['flat/recommended'],
   jestPlugin.configs['flat/recommended'],
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
